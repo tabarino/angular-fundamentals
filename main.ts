de45df36a5c9a872c736b5e6a3faedf017f5322d
tabarino <tabarino@outlook.com>
2020-09-19 18:08:10 +0100

    Bootstrapping

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+// Here goes the code to Bootstap the AppModule and setup the project to run
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
