d98194f39ef13ef4a0aed8134bf6b436329764a7
tabarino <tabarino@outlook.com>
2020-09-20 17:49:23 +0100

    Safe Navigation Operator

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface Child {
+    name: string;
+    age: number
+}
+
 interface Passenger {
     id: number;
     fullname: string;
     checkedIn: boolean;
     checkInDate?: number;
+    children: Child[] | null;
 }
 
 @Component({
@@ -25,6 +31,9 @@ interface Passenger {
                         Check in Date: 
                         {{ passenger.checkInDate ? (passenger.checkInDate | date: 'dd/MM/yyyy') : 'Not checked in' }}
                     </div>
+                    <div class="children">
+                        Children: {{ passenger.children?.length || 0 }}
+                    </div>
                 </li>
             </ul>
         </div>
@@ -35,27 +44,32 @@ export class AppComponent {
         id: 1,
         fullname: 'Stephen Doe',
         checkedIn: true,
-        checkInDate: 1490742000000
+        checkInDate: 1490742000000,
+        children: null
     }, {
         id: 2,
         fullname: 'Rose Ball',
         checkedIn: false,
-        checkInDate: null
+        checkInDate: null,
+        children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
     }, {
         id: 3,
         fullname: 'James Forde',
         checkedIn: true,
-        checkInDate: 1491606000000
+        checkInDate: 1491606000000,
+        children: null
     }, {
         id: 4,
         fullname: 'Louise Who',
         checkedIn: true,
-        checkInDate: 1488412800000
+        checkInDate: 1488412800000,
+        children: [{ name: 'Jessica', age: 1 }]
     }, {
         id: 5,
         fullname: 'Tina Tunner',
         checkedIn: false,
-        checkInDate: null
+        checkInDate: null,
+        children: null
     }];
 
     constructor() { }
