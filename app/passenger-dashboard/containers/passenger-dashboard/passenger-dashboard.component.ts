78a939a1b3a96d817e6b3d594f395834ab0f7846
tabarino <tabarino@outlook.com>
2020-09-20 22:59:40 +0100

    NgOnInit

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,39 +7,41 @@ import { Passenger } from '../../models/passenger.model';
     styleUrls: ['./passenger-dashboard.component.scss']
 })
 export class PassengerDashboardComponent implements OnInit {
-    passengers: Passenger[] = [{
-        id: 1,
-        fullname: 'Stephen Doe',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: null
-    }, {
-        id: 2,
-        fullname: 'Rose Ball',
-        checkedIn: false,
-        checkInDate: null,
-        children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-    }, {
-        id: 3,
-        fullname: 'James Forde',
-        checkedIn: true,
-        checkInDate: 1491606000000,
-        children: null
-    }, {
-        id: 4,
-        fullname: 'Louise Who',
-        checkedIn: true,
-        checkInDate: 1488412800000,
-        children: [{ name: 'Jessica', age: 1 }]
-    }, {
-        id: 5,
-        fullname: 'Tina Tunner',
-        checkedIn: false,
-        checkInDate: null,
-        children: null
-    }];
+    passengers: Passenger[];
     
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.passengers = [{
+            id: 1,
+            fullname: 'Stephen Doe',
+            checkedIn: true,
+            checkInDate: 1490742000000,
+            children: null
+        }, {
+            id: 2,
+            fullname: 'Rose Ball',
+            checkedIn: false,
+            checkInDate: null,
+            children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+        }, {
+            id: 3,
+            fullname: 'James Forde',
+            checkedIn: true,
+            checkInDate: 1491606000000,
+            children: null
+        }, {
+            id: 4,
+            fullname: 'Louise Who',
+            checkedIn: true,
+            checkInDate: 1488412800000,
+            children: [{ name: 'Jessica', age: 1 }]
+        }, {
+            id: 5,
+            fullname: 'Tina Tunner',
+            checkedIn: false,
+            checkInDate: null,
+            children: null
+        }];
+    }
 }
