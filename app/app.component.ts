b7154fc270d3134a562cb1d8f7ae5b25173ce80f
tabarino <tabarino@outlook.com>
2020-09-20 12:50:53 +0100

    Style and NgStyle

--- app/app.component.ts
+++ app/app.component.ts
@@ -36,6 +36,31 @@ interface Passenger {
                     {{ i }}: {{ passenger.fullname }}
                 </li>
             </ul>
+            <!-- Styles -->
+            <br>
+            <h3>Airline Passengers</h3>
+            <ul>
+                <li *ngFor="let passenger of passengers; let i = index;">
+                    <span 
+                        class="status" [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"
+                    ></span>
+                    {{ i }}: {{ passenger.fullname }}
+                </li>
+            </ul>
+            <!-- NgStyle -->
+            <br>
+            <h3>Airline Passengers</h3>
+            <ul>
+                <li *ngFor="let passenger of passengers; let i = index;">
+                    <span 
+                        class="status"
+                        [ngStyle]="{
+                            backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+                        }"
+                    ></span>
+                    {{ i }}: {{ passenger.fullname }}
+                </li>
+            </ul>
         </div>
     `
 })
