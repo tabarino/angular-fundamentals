92acb059a513e10d0086a0149e29943be9777aad
tabarino <tabarino@outlook.com>
2020-09-22 09:40:52 +0100

    Presentational Components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'passenger-count',
+    templateUrl: './passenger-count.component.html',
+    styleUrls: ['./passenger-count.component.scss']
+})
+export class PassengerCountComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'passenger-detail',
+    templateUrl: './passenger-detail.component.html',
+    styleUrls: ['./passenger-detail.component.scss']
+})
+export class PassengerDetailComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
