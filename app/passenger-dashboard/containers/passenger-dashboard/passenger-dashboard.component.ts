a605266354fbea4eaef0f9ef2b2904eb82074ea1
tabarino <tabarino@outlook.com>
2020-09-26 17:05:46 +0100

    Promises Alternative

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -14,12 +14,12 @@ export class PassengerDashboardComponent implements OnInit {
 
     ngOnInit() {
         this.passengerService.getPassengers()
-            .subscribe((data: Passenger[]) => this.passengers = data);
+            .then((data: Passenger[]) => this.passengers = data);
     }
 
     handleEdit(event: Passenger) {
         this.passengerService.updatePassengers(event)
-            .subscribe((data: Passenger) => {
+            .then((data: Passenger) => {
                 this.passengers = this.passengers.map((passenger: Passenger) => {
                     if (passenger.id === event.id) {
                         passenger = Object.assign({}, passenger, event);
@@ -32,7 +32,7 @@ export class PassengerDashboardComponent implements OnInit {
 
     handleRemove(event: Passenger) {
         this.passengerService.removePassengers(event)
-            .subscribe((data: Passenger) => {
+            .then((data: Passenger) => {
                 this.passengers = this.passengers.filter((passenger: Passenger) => {
                     return passenger.id !== event.id
                 });
--- app/passenger-dashboard/services/passenger-dashboard.service.ts
+++ app/passenger-dashboard/services/passenger-dashboard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { Passenger } from "../models/passenger.model";
 
 const PASSENGER_API = '/api/passengers';
@@ -10,13 +10,14 @@ const PASSENGER_API = '/api/passengers';
 export class PassengerDashboardService {
     constructor(private http: Http) { }
 
-    getPassengers(): Observable<Passenger[]> {
+    getPassengers(): Promise<Passenger[]> {
         return this.http
             .get(PASSENGER_API)
-            .map((response: Response) => response.json());
+            .toPromise()
+            .then((response: Response) => response.json());
     }
 
-    updatePassengers(passenger: Passenger): Observable<Passenger> {
+    updatePassengers(passenger: Passenger): Promise<Passenger> {
         let headers = new Headers({
             'Content-Type': 'application/json'
         });
@@ -27,12 +28,14 @@ export class PassengerDashboardService {
 
         return this.http
             .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-            .map((response: Response) => response.json());
+            .toPromise()
+            .then((response: Response) => response.json());
     }
 
-    removePassengers(passenger: Passenger): Observable<Passenger> {
+    removePassengers(passenger: Passenger): Promise<Passenger> {
         return this.http
             .delete(`${PASSENGER_API}/${passenger.id}`)
-            .map((response: Response) => response.json());
+            .toPromise()
+            .then((response: Response) => response.json());
     }
 }
