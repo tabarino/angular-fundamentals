83d83a7bd5a236d333301070b7f8539bd370e118
tabarino <tabarino@outlook.com>
2020-09-27 11:52:14 +0100

    Route Config Outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
+import { AppRoutingModule } from './app-routing.module';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.components';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         CommonModule,
+        AppRoutingModule,
         PassengerDashboardModule
     ],
     bootstrap: [AppComponent]
