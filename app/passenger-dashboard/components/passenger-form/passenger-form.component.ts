d83d4d1d331a9bb80cfe6f75863ca266d379c886
tabarino <tabarino@outlook.com>
2020-09-27 10:52:08 +0100

    Select Options and NgValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Baggage } from '../../models/baggage.model';
 import { Passenger } from '../../models/passenger.model';
 
 @Component({
@@ -8,6 +9,19 @@ import { Passenger } from '../../models/passenger.model';
 })
 export class PassengerFormComponent implements OnInit {
     @Input() detail: Passenger;
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No Baggage'
+    }, {
+        key: 'hand-only',
+        value: 'Hand Baggage'
+    }, {
+        key: 'hold-only',
+        value: 'Hold Baggage'
+    }, {
+        key: 'hand-hold',
+        value: 'Hand and Hold Baggage'
+    }];
 
     constructor() { }
 
