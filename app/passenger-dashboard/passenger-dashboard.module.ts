e3b9b2921631924f07a8ece51f99ac430e71f44c
tabarino <tabarino@outlook.com>
2020-09-27 13:32:11 +0100

    Child Dynamic Routes

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -17,7 +17,13 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 import { PassengerDashboardService } from './services/passenger-dashboard.service';
 
 const routes: Routes = [
-    { path: 'passengers', component: PassengerDashboardComponent }
+    { 
+        path: 'passengers',
+        children: [
+            { path: '', component: PassengerDashboardComponent },
+            { path: ':id', component: PassengerViewerComponent }
+        ]
+    }
 ];
 
 @NgModule({
