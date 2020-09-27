86c1d5f5e6e3dd5cdcfa09285f406899f4bddf1b
tabarino <tabarino@outlook.com>
2020-09-27 13:53:29 +0100

    Hash Location

--- app/app.module.ts
+++ app/app.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
     imports: [
         BrowserModule,
         CommonModule,
-        RouterModule.forRoot(routes),
+        RouterModule.forRoot(routes, { useHash: true }),
         PassengerDashboardModule
     ],
     bootstrap: [AppComponent]
